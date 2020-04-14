91bf3b6e4e504f1d2a96f43ab217ecf3ceebc27a
Alex Susemihl <susemihl@susemihl-macbookpro1.roam.corp.google.com>
2020-04-14 17:39:09 -0300

    Adding a Piece class

--- src/board.py
+++ src/board.py
@@ -1,8 +1,7 @@
 
-
 class Board:
-  def __init__(self):
-    pass
-  
-  def __str__(self):
-    return "This will print a board"
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "This will print a board"
--- src/board_test.py
+++ src/board_test.py
@@ -2,9 +2,9 @@ import unittest
 from board import Board
 
 class TestBoard(unittest.TestCase):
-  def test_prints(self):
-    b = Board()
-    self.assertEqual(b.__str__(), "This will print a board")
+    def test_prints(self):
+        b = Board()
+        self.assertEqual(b.__str__(), "This will print a board")
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
